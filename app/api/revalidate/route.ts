b081182366d82f9781e1983d7d0ee6f5d36534c0
Martynas <mbr@charlietango.dk>
2024-10-04 01:35:47 +0200

    enable revalidat by tag

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -4,12 +4,16 @@ import { revalidateTag } from "next/cache";
 export async function POST(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
   const secret = requestHeaders.get("x-vercel-reval-key");
+  const body = await request.json();
+  const tag = body.metadata.tags[0].sys.id;
 
   if (secret !== process.env.CONTENTFUL_REVALIDATE_SECRET) {
     return NextResponse.json({ message: "Invalid secret" }, { status: 401 });
   }
 
-  revalidateTag("hero");
-
+  if (!tag) {
+    return NextResponse.json({ message: "No tag provided" }, { status: 400 });
+  }
+  revalidateTag(tag);
   return NextResponse.json({ revalidated: true, now: Date.now() });
 }
